5ed2c40d036e3658b0ff363d75b59a1cc84210f9
Raskolnikov <jerry.tann@endress.com>
2023-02-08 17:39:19 +0800

    ✨ test hostedpage

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # place to practice chargebee ruby api
 
+0. in src
+1. set CHARGEBEE_SITE and CHARGEBEE_API_KEY env vars.
+2. ruby chargebee_test.rb
---
+++ src/chargebee_test.rb
@@ -0,0 +1,40 @@
+require 'chargebee'
+ChargeBee.configure(
+  site: ENV.fetch('CHARGEBEE_SITE'),
+  api_key: ENV.fetch('CHARGEBEE_API_KEY')
+)
+
+puts "will print coupons"
+
+coupons = ChargeBee::Coupon.list({
+    :limit=>5,
+  })
+  coupons.each do |entry|
+  puts entry.coupon
+end
+
+puts "will try hostedpage api"
+
+result = ChargeBee::HostedPage.checkout_new({
+  :billing_address => {
+    :first_name => "John",
+    :last_name => "Doe",
+    :line1 => "PO Box 9999",
+    :city => "Walnut",
+    :state => "California",
+    :zip => "91789",
+    :country => "US"
+    },
+  :subscription => {
+    :plan_id => "no_trial"
+    },
+  :customer => {
+    :email => "john@user.com",
+    :first_name => "John",
+    :last_name => "Doe",
+    :locale => "fr-CA",
+    :phone => "+1-949-999-9999"
+    }
+  })
+
+  puts result
